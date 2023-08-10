05eae28e3df9c98d9194086e521fdf7ecb7a1005
Prady555555 <139098258+Prady555555@users.noreply.github.com>
2023-08-10 12:47:26 +0530

    Add files via upload

---
+++ ownProject/index.js
@@ -0,0 +1,24 @@
+function main(){
+var random1 = Math.floor(Math.random()*6)+1;
+var picture = "../project-1/Dicee Challenge - Starting Files/Dicee Challenge - Starting Files/images/dice"+random1+".png";
+var image1 = document.querySelectorAll("img")[0].setAttribute("src",picture);
+
+var random2 = Math.floor(Math.random()*6)+1;
+var picture = "../project-1/Dicee Challenge - Starting Files/Dicee Challenge - Starting Files/images/dice"+random2+".png";
+var image2 = document.querySelectorAll("img")[1].setAttribute("src",picture);
+
+if(random1>random2){
+  document.querySelectorAll('p')[0].innerHTML="Player 1 has been Won"
+}else if(random2>random1){
+     document.querySelectorAll('p')[1].innerHTML="Player 2 has been Won"
+}else{
+     document.querySelector("h1").innerHTML="DRAW"
+}
+}
+
+main();
+
+
+
+
+
